ecd57a068b83751e30809e7afc6e2540caf11c34
Hamidreza Pakpour <Hamidrezapakpour95@gmail.com>
2025-03-21 18:17:29 +0330

    Nunit test sample for calculator

---
+++ StaticSamplesUnitTest/CalculatorNUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using StaticSamples;
+
+namespace StaticSamplesUnitTest
+{
+    [TestFixture]
+    public class CalculatorNUnitTest
+    {
+        [Test]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            //Arrange
+            Calculator calculator = new Calculator();
+
+            //Act
+            int result = calculator.Sum(10, 20);
+
+            //Assert
+            ClassicAssert.AreEqual(30, result);
+            // always should write like this : (expected result , actual result)
+        }
+    }
+}
